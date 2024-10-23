6a8d800b9eef8ed244846dbb1ccefe4b94e42567
Franco Ledesma <francojavierledesma17@gmail.com>
2024-10-23 10:14:09 -0300

    se agrega clase conexion
    driver de conexion a BD

---
+++ src/appfinal_grupo7/AccesoADatos/Conexion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appfinal_grupo7.AccesoADatos;
+
+/**
+ *
+ * @author Franquito Ledesma
+ */
+public class Conexion {
+    
+}
